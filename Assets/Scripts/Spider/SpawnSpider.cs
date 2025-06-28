6b6b3ce075b45fad39b6a74819632a0f7e3ba450
Kilian Armas Pérez <kilamper@gmail.com>
2025-06-28 22:21:44 +0100

    feat: Add initial position and rotation handling for spider prefab

--- Assets/Scripts/Spider/SpawnSpider.cs
+++ Assets/Scripts/Spider/SpawnSpider.cs
@@ -3,9 +3,23 @@ using UnityEngine;
 public class SpawnSpider : MonoBehaviour
 {
     [SerializeField] private GameObject spiderPrefab;
+    private static Vector3 initialPosition;
+    private static Vector3 initialRotation;
+
+    private void Start()
+    {
+        initialPosition = spiderPrefab.transform.position;
+        initialRotation = spiderPrefab.transform.eulerAngles;
+    }
 
     public void showSpiderModel()
     {
         spiderPrefab.SetActive(!spiderPrefab.activeSelf);
+    }
+
+    public void moveSpiderToInitialPosition()
+    {
+        spiderPrefab.transform.position = initialPosition;
+        spiderPrefab.transform.eulerAngles = initialRotation;
     }
 }
